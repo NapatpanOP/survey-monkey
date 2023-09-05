231ab65d7b1289328b6e6e918d8bb0df159334bc
NapatpanOP <napatpanns424@gmail.com>
2023-09-05 22:37:42 +0700

    Add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ["./*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      'primary-color': 'rgb(249, 190, 0)',
+      'secondary-color': 'rgb(34, 79, 60)',
+      'midnight': 'rgb(30, 33, 36)',
+      'gray': 'rgb(115, 116, 122)'
+    },
   },
   plugins: [],
 }
